44dd8b8b46561b494216385da2e41fc66f87f183
eliffisik <eliffisik@icloud.com>
2023-05-30 19:32:11 +0300

    feedscreenupdated

--- lib/Screens/FeedScreen.dart
+++ lib/Screens/FeedScreen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-
 import './HomeScreen.dart';
 import './SearchScreen.dart';
 import './ProfileScreen.dart';
+import './FavoriteScreen.dart';
 
 class FeedScreen extends StatefulWidget {
   final String currentUserId;
@@ -17,18 +17,18 @@ class _FeedScreenState extends State<FeedScreen> {
   int _selectedTab = 0;
 
   List<Widget> _screens = [
-    HomeScreen(), // Pass currentUserId here
+    HomeScreen(),
     SearchScreen(),
+    FavoriteScreen(),
     ProfileScreen(),
   ];
-  
-  static get currentUserId => null;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _screens[_selectedTab],
       bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed, // Bu satırı ekleyin
         onTap: (index) {
           setState(() {
             _selectedTab = index;
@@ -36,11 +36,25 @@ class _FeedScreenState extends State<FeedScreen> {
         },
         currentIndex: _selectedTab,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
+          ),
         ],
         selectedItemColor: Color.fromARGB(255, 129, 82, 169),
+        unselectedItemColor: Color.fromARGB(255, 129, 82, 169),
       ),
     );
   }
