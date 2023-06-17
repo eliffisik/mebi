530773e574840c718eab354a43c98cb8efe6933d
eliffisik <eliffisik@icloud.com>
2023-06-17 10:01:29 +0300

    registerscreenconnnectedtobackend

--- lib/Screens/RegistrationScreen.dart
+++ lib/Screens/RegistrationScreen.dart
@@ -1,9 +1,9 @@
-
 import 'package:flutter/material.dart';
-
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import '../RoundedButton.dart';
-
+import 'WelcomeScreen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   @override
@@ -11,9 +11,77 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  late String _firstName;
+  late String _lastName;
+  late String _description;
   late String _email;
+  late String _userName;
   late String _password;
-  late String _name;
+  late String _confirmPassword;
+
+  void openWebView(String url) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => WebViewScreen(url: url),
+      ),
+    ).then((value) {
+      navigateToWelcomeScreen();
+    });
+  }
+
+  Future<void> registerUser() async {
+    String apiUrl = 'https://clean-architecture.azurewebsites.net/api/Account/register';
+    print(apiUrl);
+
+    try {
+      var response = await http.post(
+        Uri.parse(apiUrl),
+        headers: {
+          "content-type": "application/json",
+          "accept": "*/*",
+        },
+        body: jsonEncode({
+          'firstName': _firstName,
+          'lastName': _lastName,
+          "description": _description,
+          'email': _email,
+          'userName': _userName,
+          'password': _password,
+          'confirmPassword': _confirmPassword,
+        }),
+      );
+      print(response.statusCode);
+      if (response.statusCode == 200) {
+        var path = response.body.substring(135);
+        print(response.body);
+        String confirmEmailUrl = 'http://localhost:57561/api/account/confirm-email/?userId=4722365b-a8da-4c05-a9f9-241aafd62041&code=Q2ZESjhISWFXa0ZIdjZwSXJrSWtZQVZVcWtPV0pROXo1bXZQcGpRemtOTzMyaGI3TGNMV3BpbFNEN1MvcWw0ZlJmODJLME0vNjRJZHlHTERPMTJQZ0JYaEtnRStCZCt4VUVqTitqTVlNcHdqTkN4RTFWSGozVUd4ZlBOOVVaM0VTUnRHYVRSQWp3UVNVRHJCNUxnVmFuMXAvVXBZbzhlK28yWmVMMnpqNHFsUCtsenlUTDM4cFBXZUhFQjFDRkpKb29LUEpxOWh1K1dLeml6NkN6ZjVqeFJpRXFtZzVCaWpsd3g2ekFjVHhtVFBzeUpRUWlWRWFnOGNtNnNvQ3RPWEhXcHNhQT09';
+        openWebView(confirmEmailUrl);
+
+        // Başarılı kayıt durumunda yapılması gereken işlemler
+        // Return the response object
+      } else {
+        print('Registration failed: ${response.body}');
+
+        // Kayıt başarısız olduğunda yapılması gereken işlemler
+        // Return null when registration fails
+      }
+    } catch (e) {
+      print('Error during registration: $e');
+      // Kayıt işlemi sırasında bir hata oluştuğunda yapılması gereken işlemler
+      // Return null when an error occurs during registration
+    }
+  }
+
+  void navigateToWelcomeScreen() {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (context) => WelcomeScreen(),
+      ),
+      (route) => false,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,12 +113,38 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 ),
                 onChanged: (value) {
                   setState(() {
-                    _name = value;
+                    _firstName = value;
                   });
                 },
               ),
               SizedBox(
-                height: 30,
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Surname',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _lastName = value;
+                  });
+                },
+              ),
+                SizedBox(
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Description',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _description = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20,
               ),
               TextField(
                 decoration: InputDecoration(
@@ -63,7 +157,20 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 },
               ),
               SizedBox(
-                height: 40,
+                height: 20,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Username',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _userName = value;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20,
               ),
               TextField(
                 obscureText: true,
@@ -77,18 +184,74 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                 },
               ),
               SizedBox(
+                height: 20,
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Confirm Password',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _confirmPassword = value;
+                  });
+                },
+              ),
+              SizedBox(
                 height: 50,
               ),
               RoundedButton(
-                btnText: 'Create account',
+                btnText: 'Create Account',
                 onBtnPressed: () {
-                  // Implement your logic for creating a user account here
-                  // You can use the entered values (_name, _email, _password) to register the user
+                  registerUser();
                 },
               ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  final String url;
+
+  WebViewScreen({required this.url});
+
+  @override
+  _WebViewScreenState createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  InAppWebViewController? _webViewController;
+
+  void navigateToWelcomeScreen() {
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (context) => WelcomeScreen(),
+      ),
+      (route) => false,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // WebView AppBar özellikleri
+      ),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(url: Uri.parse(widget.url)),
+        onWebViewCreated: (controller) {
+          _webViewController = controller;
+        },
+        onLoadStop: (controller, url) {
+          if (url == widget.url) {
+            navigateToWelcomeScreen();
+          }
+        },
       ),
     );
   }
