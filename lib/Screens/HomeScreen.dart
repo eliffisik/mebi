051a5ed05971f7baf8893a52310d056a842c22b4
eliffisik <eliffisik@icloud.com>
2023-06-17 10:17:34 +0300

    Homescreenedited

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -6,44 +6,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<bool> _likedStatus = [false, false, false, false, false]; // Beğeni durumunu tutan bir liste
+  List<bool> _likedStatus = [false, false, false, false, false, false, false];
+
+  List<String> senders = [
+    'Elif Isik',
+    'Burcu Acan',
+    'Mustafa Yencilek',
+    'Bob John',
+    'Emma H',
+    'Michael Brown',
+    'David Wilson'
+  ];
+  List<String> messages = [
+    'This is my first message!',
+    'Hello, how are you?',
+    'Just wanted to say hi!',
+    'What are your plans for today?',
+    'Have a great day!',
+    'Are you available for a meeting tomorrow?',
+    'I hope you had a nice weekend.'
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
-          actions: [
-         
-          PopupMenuButton<String>(
-            color: Color.fromARGB(255, 238, 218, 224),
-            onSelected: (String value) {
-              // Seçilen değeri kullanmak için gerekli işlemleri burada yapabilirsiniz
-              print(value);
-            },
-            itemBuilder: (BuildContext context) {
-              return <PopupMenuEntry<String>>[
-             
-                PopupMenuItem<String>(
-                  value: 'Settings',
-                  child: Text('Settings'),
-                  
-                ),
-                PopupMenuItem<String>(
-                  value: 'Log Out',
-                  child: Text('Log Out'),
-                ),
-              
-              ];
-            },
-          ),
-        ],
-          backgroundColor:Color.fromARGB(255, 129, 82, 169),
+        backgroundColor: Color.fromARGB(255, 129, 82, 169),
       ),
       body: ListView.builder(
-        itemCount: 5, // Değiştirilmesi gereken öğe sayısı
+        itemCount: senders.length,
         itemBuilder: (context, index) {
-          return _buildMessageCard('John Doe', 'This is my first message!', index);
+          return _buildMessageCard(senders[index], messages[index], index);
         },
       ),
     );
@@ -57,11 +51,13 @@ class _HomeScreenState extends State<HomeScreen> {
         trailing: IconButton(
           icon: Icon(
             _likedStatus[index] ? Icons.favorite : Icons.favorite_border,
-            color: _likedStatus[index] ? Color.fromARGB(255, 164, 37, 189) : null,
+            color: _likedStatus[index]
+                ? Color.fromARGB(255, 164, 37, 189)
+                : null,
           ),
           onPressed: () {
             setState(() {
-              _likedStatus[index] = !_likedStatus[index]; 
+              _likedStatus[index] = !_likedStatus[index];
             });
           },
         ),
